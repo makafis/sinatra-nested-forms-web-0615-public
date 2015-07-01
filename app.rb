cf59511582b3c320645cd033638edfdb5d747e5f
makafis <vinnyalfieri@gmail.com>
2015-07-01 15:13:45 -0400

    complete

--- app.rb
+++ app.rb
@@ -4,6 +4,22 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :'/root' 
+    end
 
-  end
-end
+
+    get '/new' do 
+      erb :'/pirates/new' 
+    end
+
+    post '/pirates' do 
+    @pirate = params[:pirate]
+      erb :'/pirates/show' 
+    end
+
+
+
+
+  end #end class
+end #module end
